44f7e851fa3eb095ec6fd639a473f1ceb42c374c
Shemar Dacosta <dshemar6@gmail.com>
2018-11-20 17:27:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ function updateObjectWithKeyAndValue (recipes, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue (recipes, key, value){
-  return Object.assign({},recipes,{[key]:value})
+  recipes[key] = value
+  return recipes
 }
